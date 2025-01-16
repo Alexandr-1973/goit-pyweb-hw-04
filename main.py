3a806d68fb11c99100f0bc35cc9f095fb7ff0a1a
Alexandr-1973 <144156771+Alexandr-1973@users.noreply.github.com>
2025-01-16 15:42:41 +0200

    1task-fin

---
+++ main.py
@@ -0,0 +1,112 @@
+import mimetypes
+import pathlib
+import socket
+import json
+import logging
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import urllib.parse
+from threading import Thread
+from datetime import datetime
+
+BASE_DIR = pathlib.Path()
+HTTP_PORT = 3000
+HTTP_HOST = '0.0.0.0'
+SOCKET_HOST = '127.0.0.1'
+SOCKET_PORT = 5000
+storage_path = "storage/data.json"
+
+class HttpHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        pr_url = urllib.parse.urlparse(self.path)
+        if pr_url.path == '/':
+            self.send_html_file('index.html')
+        elif pr_url.path == '/message':
+            self.send_html_file('message.html')
+        else:
+            if pathlib.Path().joinpath(pr_url.path[1:]).exists():
+                self.send_static()
+            else:
+                self.send_html_file('error.html', 404)
+
+    def do_POST(self):
+        size = self.headers.get('Content-Length')
+        data = self.rfile.read(int(size))
+
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        client_socket.sendto(data, (SOCKET_HOST, SOCKET_PORT))
+        client_socket.close()
+
+        self.send_response(302)
+        self.send_header('Location', '/')
+        self.end_headers()
+
+    def send_html_file(self, filename, status=200):
+        self.send_response(status)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        with open(filename, 'rb') as fd:
+            self.wfile.write(fd.read())
+
+    def send_static(self):
+        self.send_response(200)
+        mt = mimetypes.guess_type(self.path)
+        if mt:
+            self.send_header("Content-type", mt[0])
+        else:
+            self.send_header("Content-type", 'text/plain')
+        self.end_headers()
+        with open(f'.{self.path}', 'rb') as file:
+            self.wfile.write(file.read())
+
+def save_data_from_form(data):
+    parse_data = urllib.parse.unquote_plus(data.decode())
+    try:
+        parse_dict = {key: value for key, value in [el.split('=') for el in parse_data.split('&')]}
+        json_dict_part={str(datetime.now()):parse_dict}
+
+        if not pathlib.Path(storage_path).exists():
+            with open(storage_path, 'w', encoding='utf-8') as file:
+                json.dump(json_dict_part, file, ensure_ascii=False, indent=4)
+        else:
+            with open(storage_path, 'r', encoding='utf-8') as file:
+                json_dict = json.load(file)
+                json_dict.update(json_dict_part)
+            with open(storage_path, 'w', encoding='utf-8') as file:
+                json.dump(json_dict, file, ensure_ascii=False, indent=4)
+
+    except ValueError as err:
+        logging.error(err)
+    except OSError as err:
+        logging.error(err)
+
+def run_socket_server(host, port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind((host, port))
+    logging.info("Starting socket server")
+    try:
+        while True:
+            info, address = server_socket.recvfrom(65535)
+            logging.info(f"Socket received {address}: {info}")
+            save_data_from_form(info)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        server_socket.close()
+
+def run(server_class=HTTPServer, handler_class=HttpHandler):
+    server_address = ('', 3000)
+    http = server_class(server_address, handler_class)
+    try:
+        http.serve_forever()
+    except KeyboardInterrupt:
+        http.server_close()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, format='%(threadName)s %(message)s')
+    server = Thread(target=run)
+    server.start()
+    server_socket = Thread(target=run_socket_server, args=(SOCKET_HOST, SOCKET_PORT))
+    server_socket.start()
+
+
